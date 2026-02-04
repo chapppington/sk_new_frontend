70c1c4a904a5d20a6714854595c87da994bc1b3c
chapppington <stas.mega.96@bk.ru>
2026-02-04 11:50:21 +0700

    chore: small refactor

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next"
 import seoService from "@/services/seo/seo.service"
-import Home from "./Home"
+import HomeClientComponent from "./Home"
 
 export async function generateMetadata(): Promise<Metadata> {
   return seoService.getMetadata("/", { next: { revalidate: 60 } })
 }
 
 export default function HomePage() {
-  return <Home />
+  return <HomeClientComponent />
 }
